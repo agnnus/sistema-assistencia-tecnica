db09a840fd6c3dde89368db765bb5a4d1a926272
Agnnus Ryan <ryanalmeida10270@gmail.com>
2026-09-25 09:58:45 -0300

    Completar cadastro de clientes

--- clientes.js
+++ clientes.js
@@ -17,6 +17,34 @@ const cliente = {
 entrada.question("Qual é o seu nome?",
     (resposta) => {
         cliente.nome = resposta;
-        console.log(cliente);
-    });
+
+        entrada.question("Qual é o seu telefone?",
+            (resposta) => {
+                cliente.telefone = resposta;
+
+                entrada.question("Qual o seu email?", (resposta) => {
+                    cliente.email = resposta;
+
+                    entrada.question("Qual é o aparelho?", (resposta) => {
+                        cliente.aparelho = resposta;
+
+                        entrada.question("Qual é a marca do aparelho?", (resposta) => {
+                            cliente.marca = resposta;
+
+                            entrada.question("Qual é o modelo do aparelho?", (resposta) => {
+                                cliente.modelo = resposta;
+
+                                entrada.question("Qual é o problema do aparelho?", (resposta) => {
+                                    cliente.problema = resposta; 
+
+                                    console.log(cliente);
+                                });
+                            });
+                        });
+                    });
+                });
+            });
+         });
+
+
 
